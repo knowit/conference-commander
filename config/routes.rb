d1c701f1855d4da5c7100dead893f87804f6f8db
S. Christoffer Eliesen <christoffer@eliesen.no>
2016-11-08 18:43:13 +0100

    pages/index => pages/front. Redirect pages/front to "/".

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+
+  # we need to redirect in order to use the correct method and thus the correct layout:
+  get '/pages/front', to: redirect('/')
   get '/pages/:page' => 'pages#show'
-  root to: 'pages#index'
+
+  root to: 'pages#front'
 end
