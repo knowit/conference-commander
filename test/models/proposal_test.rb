2dd7909e7ac7cff6ad4939c3e030ab9f7e7955e8
Henning Wold <henwol@knowit.no>
2017-01-17 16:19:23 +0100

    Updates annotations and fixes error in seed file

--- test/models/proposal_test.rb
+++ test/models/proposal_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: proposals
+#
+#  id          :integer          not null, primary key
+#  title       :text             not null
+#  description :text             not null
+#  duration    :integer          not null
+#  user_id     :integer          not null
+#  event_id    :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_proposals_on_event_id  (event_id)
+#  index_proposals_on_user_id   (user_id)
+#
+
 require 'test_helper'
 
 class ProposalTest < ActiveSupport::TestCase
