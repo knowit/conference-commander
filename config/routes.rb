c581fedd1bb734808fb5d24a0b9c58fd0c090e8d
S. Christoffer Eliesen <christoffer@eliesen.no>
2017-01-17 17:06:02 +0100

    Accommodation CRUD.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   resources :users
+  resources :accommodations
 
   # we need to redirect in order to use the correct method and thus the correct layout:
   get '/pages/front', to: redirect('/')
