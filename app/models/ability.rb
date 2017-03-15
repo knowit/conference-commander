5b1dd7b32dd07a8fd04ba05b4f0ec077b85c7286
S. Christoffer Eliesen <christoffer@eliesen.no>
2017-03-15 15:49:26 +0100

    models/ability: Define abilities. Allow users to manage themselves.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,37 @@ class Ability
   def initialize(user)
     @user = user
     guest_abilities
-    if user && user.role == 'administrator'
-      administrator_abilities
+    return unless user
+
+    common_abilities
+
+    # roles are defined in user model
+    case user.role
+    when 'administrator' administrator_abilities
+    when 'organizer' organizer_abilities
+    when 'speaker' speaker_abilities
+    when 'participant' participant_abilities
+    end
+  end
+
+  def common_abilities
+    can :manage, [User] do |user|
+      # allow users to manage themselves
+      @user == user
     end
   end
 
   def administrator_abilities
     can :manage, :all
+  end
+
+  def organizer_abilities
+  end
+
+  def speaker_abilities
+  end
+
+  def participant_abilities
   end
 
   def guest_abilities
