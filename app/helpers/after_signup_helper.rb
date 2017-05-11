aba9ceddd94557a95d6e8556ef6be6eb897badfc
Anders Aarsæther <aaa@knowit.no>
2017-05-11 18:34:58 +0200

    Updates stepper

--- app/helpers/after_signup_helper.rb
+++ app/helpers/after_signup_helper.rb
@@ -1,2 +1,15 @@
 module AfterSignupHelper
+  def step_classes(step)
+    classes = []
+    classes << case
+    when past_step?(step)
+      'completed'
+    when future_step?(step)
+      'future'
+    end
+
+    classes << 'active' if step == params[:id].to_sym
+    classes << 'step'
+    classes.join(' ')
+  end
 end
