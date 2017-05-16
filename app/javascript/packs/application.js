e229b43ae0be8515b058da6986ca156d5ec69f2b
Anders Aarsæther <aaa@knowit.no>
2017-05-16 15:14:55 +0200

    Passport name should be split into first and last name

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,6 @@
 import { calendar } from 'semantic-ui-calendar/dist/calendar';
 
 $(document).on('turbolinks:load', function() {
-  $('.calendar').calendar({type: 'date'})
+  $('.calendar').calendar({ type: 'date' });
   $('.ui.dropdown').dropdown();
 })
--- db/migrate/20170511131441_add_fields_to_user.rb
+++ db/migrate/20170511131441_add_fields_to_user.rb
@@ -1,6 +1,7 @@
 class AddFieldsToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :passport_name, :string
+    add_column :users, :passport_first_name, :string
+    add_column :users, :passport_last_name, :string
     add_column :users, :passport_number, :string
     add_column :users, :passport_issued_at, :date
     add_column :users, :passport_expires_at, :date
