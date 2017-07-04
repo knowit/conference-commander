4969ef54e5d49af6c6b88d61a7266a51b5719557
S. Christoffer Eliesen <christoffer@eliesen.no>
2017-07-04 14:56:13 +0200

    events/_menu: Show list of activites. Show event menu.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -38,7 +38,7 @@ class Ability
 
   def participant_abilities
     common_abilities
-    can :read, [Event, Participation, Accommodation, Hotel]
+    can :read, [Event, Participation, Accommodation, Hotel, Activity]
     can :manage, [Participation] do |p|
       p.user.nil? || p.user == @user # allow users to manage own participation
     end
