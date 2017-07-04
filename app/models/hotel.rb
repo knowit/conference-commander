554b96cab8d8150abc244a958e8d05a254162a28
Rune Melhus <rme@knowit.no>
2017-07-04 17:56:25 +0200

    Google static map

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,4 @@
+
 # == Schema Information
 #
 # Table name: hotels
@@ -17,4 +18,17 @@ class Hotel < ApplicationRecord
   belongs_to :event
   has_many :images, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :images
+
+  geocoded_by :full_address   # can also be an IP address
+
+  after_validation :geocode          # auto-fetch coordinates
+
+  def full_address
+    [address, country].join(', ')
+  end
+
+  def to_s
+    name
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704144616) do
+ActiveRecord::Schema.define(version: 20170704152040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,11 +109,11 @@ ActiveRecord::Schema.define(version: 20170704144616) do
     t.string "name", null: false
     t.string "address"
     t.string "country"
-    t.float "lat"
-    t.float "lon"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.bigint "event_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["event_id"], name: "index_hotels_on_event_id"
   end
 
@@ -194,8 +194,7 @@ ActiveRecord::Schema.define(version: 20170704144616) do
     t.integer "gender", default: 0, null: false
     t.text "allergies"
     t.string "last_name", default: "X", null: false
-    t.string "passport_first_name"
-    t.string "passport_last_name"
+    t.string "passport_name"
     t.string "passport_number"
     t.date "passport_issued_at"
     t.date "passport_expires_at"
