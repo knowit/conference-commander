f22689c3fe91c60044c94e36b9541c975a9f55be
Anders Aarsæther <aaa@knowit.no>
2017-09-27 19:59:37 +0200

    Updates to menu access control

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,8 +2,8 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'left menu'
     primary.item :front_page, 'Front page', root_path, class: 'item'
-    primary.item :events, Event.model_name.human(count: 2), events_path, class: 'item'
-    primary.item :users, User.model_name.human(count: 2), users_path, class: 'item'
+    primary.item :events, Event.model_name.human(count: 2), events_path, class: 'item', if: -> { can?(:read, Event) }
+    primary.item :users, User.model_name.human(count: 2), users_path, class: 'item', if: -> { can?(:manage, User) }
   end
 end
 
