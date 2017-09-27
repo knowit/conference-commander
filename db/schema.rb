b41936b04f81660620d29587bfa4e2763634eff2
Henning Wold <henwol@knowit.no>
2017-09-27 16:23:41 +0200

    Adds XLSX show for Event showing all participants.

--- db/schema.rb
+++ db/schema.rb
@@ -227,8 +227,7 @@ ActiveRecord::Schema.define(version: 20170927125050) do
     t.integer "gender", default: 0, null: false
     t.text "allergies"
     t.string "last_name", default: "X", null: false
-    t.string "passport_first_name"
-    t.string "passport_last_name"
+    t.string "passport_name"
     t.string "passport_number"
     t.date "passport_issued_at"
     t.date "passport_expires_at"
