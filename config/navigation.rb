20cfe021dd8c31ab4e047a39f7dc63f053a974b0
maynkj <mayn.kjaer@gmail.com>
2017-10-04 11:31:01 +0200

    Clean up view locals.

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,9 +1,8 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'left menu'
-    primary.item :front_page, 'Front page', root_path, class: 'item'
+    primary.item :front_page, t('menu.frontpage'), root_path, class: 'item'
     primary.item :events, Event.model_name.human(count: 2), events_path, class: 'item', if: -> { can?(:read, Event) }
     primary.item :users, User.model_name.human(count: 2), users_path, class: 'item', if: -> { can?(:manage, User) }
   end
 end
-
