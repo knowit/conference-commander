2d684b1c4cf547b1ab94036791ad6a4ada803790
Roy Paulsen <rp@knowit.no>
2017-11-08 00:06:39 +0100

    Add more intelligence to the seed of data. - Events needs to be published to be visible for users that are not admins - Pick participants among the generated users, which makes it ok to run the db:seed multiple times to fill up db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,78 @@
 random = Random.new
 
-# Create users
-print "Creating users: "
+print "Creating Users: "
+users_created = []
 100.times do
-  User.create(
+  users_created << User.create(
     first_name: Faker::Name::first_name,
     last_name: Faker::Name::last_name,
-    email: Faker::Internet::email
+    email: Faker::Internet::email,
+    gender: random.rand(0..2),
+    role: random.rand(0..3),
   )
-  print 'u'
+  print '•'
 end
 puts
 
-# Create events
-print "Creating events: "
+events_created = []
+print "Creating Events: "
 3.times do
   city = Faker::Address.city
   start_date = Faker::Date.forward(365)
   end_date = Faker::Date.between(start_date, start_date + 4.days)
 
-  e = Event.create(
+  events_created << e = Event.create(
     name: city,
     description: "Fagseminar i #{city}",
     starting_at: start_date,
-    ending_at: end_date
+    ending_at: end_date,
+    is_published: random.rand(100) >= 25,
   )
   e.add_accommodations(random.rand(10) + 3, 1)
   e.add_accommodations(random.rand(15) + 3, 2)
-  print 'e'
+  print '•'
 end
 puts
 
-Event.all.each do |e|
-  puts e.name
+events_created.each do |e|
+  puts " Event: #{e.name}: ".center(50, "=")
   # Create Hotels
-  print "  Creating Hotels: "
-  Hotel.create(
-    name: "#{e.name} Hotel",
-    address: Faker::Address.street_address,
-    country: Faker::Address.country,
-    latitude: Faker::Address.latitude,
-    longitude: Faker::Address.longitude,
-    event: e
-  )
-  print 'h'
+  print " - Creating Hotels: "
+  random.rand(1..3).times do
+    Hotel.create(
+      name: "#{e.name} Hotel",
+      address: Faker::Address.street_address,
+      country: Faker::Address.country,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+      event: e,
+    )
+    print '•'
+  end
   puts
 
-  print "  Creating Venues and Tracks: "
-  3.times do
+  print " - Creating Venues and Tracks: "
+  random.rand(2..5).times do
     venue = Venue.create(
-      name: "Room #{(100..200).to_a.sample}",
+      name: "Room #{random.rand(100..200)}",
       description: Faker::Lorem.paragraph(2),
-      capacity: (20..200).to_a.sample.round(-1), # Nearest 10
-      event: e
+      capacity: random.rand(20..200).round(-1), # Nearest 10
+      event: e,
     )
-    (1..3).to_a.sample.times do 
+    print 'V'
+    random.rand(1..3).times do
       Track.create(
         name: Faker::Movie.quote,
-        venue: venue
+        venue: venue,
       )
-      print 't'
+      print '•'
     end
-    print 'v'
-  end 
+  end
   puts
 
-  print "  Creating Sessions: "
-  20.times do
-    start_time = Faker::Date::between(e.starting_at, e.ending_at) + (8..18).to_a.sample.hours
+  print " - Creating Sessions: "
+  random.rand(10..20).times do
+    start_time = Faker::Date::between(e.starting_at, e.ending_at) + random.rand(8..18).hours
     duration = [10, 30, 90].sample
     EventSession.create(
       title: Faker::Name.title,
@@ -78,27 +83,27 @@ Event.all.each do |e|
       start_time: start_time,
       outline: Faker::Lorem.paragraph(5),
       track: Track.order("RANDOM()").first,
-      state: [:proposed, :rejected, :accepted].sample
+      state: [:proposed, :rejected, :accepted].sample,
     )
-    print 's'
+    print '•'
   end
   puts
 
-  print "  Creating Activities: "
-  8.times do |i|
+  print " - Creating Activities: "
+  random.rand(5..10).times do |i|
     Activity.create(
       event: e,
       title: "#{i} #{Faker::Movie.quote}",
-      description: Faker::Lorem.paragraph
+      description: Faker::Lorem.paragraph,
     )
-    print 'a'
+    print '•'
   end
   puts
 
-  print "  Creating Participations: "
-  User.all.each do |user|
+  print " - Creating Participations: "
+  users_created.sample(random.rand(50..100)).each do |user|
     Participation.create(user: user, event: e, activities: Activity.order("RANDOM()").limit(4))
-    print 'p'
+    print '•'
   end
   puts
 end
